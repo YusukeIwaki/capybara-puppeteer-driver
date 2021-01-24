242c994f677dcfba4a066d7d6d55983f8fd2fb6b
YusukeIwaki <iwaki@i3-systems.com>
2021-01-24 23:51:25 +0900

    bundle gem capybara-puppeteer-driver

---
+++ lib/capybara/puppeteer/driver.rb
@@ -0,0 +1,10 @@
+require "capybara/puppeteer/driver/version"
+
+module Capybara
+  module Puppeteer
+    module Driver
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capybara/puppeteer/driver/version.rb
@@ -0,0 +1,7 @@
+module Capybara
+  module Puppeteer
+    module Driver
+      VERSION = "0.1.0"
+    end
+  end
+end
